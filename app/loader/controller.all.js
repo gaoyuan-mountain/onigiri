c1f4bcd30ca9e6d468a456fc013572a6acbe8fa6
gaoyuan <gaoyuancd@nhn.com>
2015-09-10 18:29:38 +0800

    Issue create and edit
    improve issue management

--- app/loader/controller.all.js
+++ app/loader/controller.all.js
@@ -1,11 +1,19 @@
+'use strict';
+
 import loginController from '../login/login.controller';
 import projectsController from '../projects/projects.controller';
 import headerController from '../common/header/header.controller';
+import issuesListController from '../issues/list/list.controller';
+import issuesCreateController from '../issues/create/create.controller';
+import issuesDetailController from '../issues/detail/detail.controller';
 
 module.exports = {
 	inject: function () {
 		loginController.inject();
 		projectsController.inject();
 		headerController.inject();
+		issuesListController.inject();
+		issuesCreateController.inject();
+		issuesDetailController.inject();
 	}
 };
